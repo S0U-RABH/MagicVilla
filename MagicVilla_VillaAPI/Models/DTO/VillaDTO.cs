baaa5a18c1246646009bedc0ae5e5510380288b9
sonus <sonus@SOURABH>
2023-09-05 22:34:30 +0530

    Add project files.

---
+++ MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicVilla_VillaAPI.Models.DTO
+{
+    public class VillaDTO
+    {
+        /*
+         A data transfer object (DTO) is an object that carries data between processes. 
+         You can use this technique to facilitate communication between two systems (like an API and your server)
+         */
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
